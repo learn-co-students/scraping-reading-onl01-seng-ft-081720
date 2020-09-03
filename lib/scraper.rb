001adb2fda201aa50cd8bd8207f7bf03c30f6bf5
mtrimner <matt.trimner@gmail.com>
2020-09-03 16:51:09 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,8 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+doc.css("#c5e14fae-0f74-578a-bfe8-a8e57884c64d" .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC").text.strip
+
+puts doc
